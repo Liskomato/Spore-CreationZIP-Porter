811207fb6795854e3d8a50e2427b6677ce7e986f
Tenderi4 <71285540+Tenderi4@users.noreply.github.com>
2023-08-08 23:14:23 +0300

    First research unto subject.
    Making a detour of SavePNG is so that I can find if it gets called when I drag a new PNG into the game.

---
+++ SporeCreationZIPPorter/dllmain.cpp
@@ -0,0 +1,61 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "stdafx.h"
+
+void Initialize()
+{
+	// This method is executed when the game starts, before the user interface is shown
+	// Here you can do things such as:
+	//  - Add new cheats
+	//  - Add new simulator classes
+	//  - Add new game modes
+	//  - Add new space tools
+	//  - Change materials
+}
+
+member_detour(SavePNG_detour, App::Thumbnail_cImportExport, bool(Resource::ResourceObject*, RenderWare::Raster*, Resource::Database*, bool, bool)) {
+	bool detoured(Resource::ResourceObject * pResource, RenderWare::Raster * pImage, Resource::Database * database,
+		bool forceReplace = false, bool disableSteganography = false) {
+
+		return original_function(this,pResource,pImage,database,forceReplace,disableSteganography);
+	}
+};
+
+
+void Dispose()
+{
+	// This method is called when the game is closing
+}
+
+void AttachDetours()
+{
+	// Call the attach() method on any detours you want to add
+	// For example: cViewer_SetRenderType_detour::attach(GetAddress(cViewer, SetRenderType));
+	SavePNG_detour::attach(GetAddress(App::Thumbnail_cImportExport, SavePNG));
+}
+
+
+// Generally, you don't need to touch any code here
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+					 )
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		ModAPI::AddPostInitFunction(Initialize);
+		ModAPI::AddDisposeFunction(Dispose);
+
+		PrepareDetours(hModule);
+		AttachDetours();
+		CommitDetours();
+		break;
+
+	case DLL_PROCESS_DETACH:
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+		break;
+	}
+	return TRUE;
+}
+
