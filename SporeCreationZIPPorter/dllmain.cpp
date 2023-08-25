4eeed2cdc6533a34e78be223a52c2e21960f2307
Tenderi4 <71285540+Tenderi4@users.noreply.github.com>
2023-08-25 23:31:20 +0300

    Forgot to disable ManualBreakpoint
    This shouldn't have happened.

--- SporeCreationZIPPorter/dllmain.cpp
+++ SporeCreationZIPPorter/dllmain.cpp
@@ -164,7 +164,7 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
-		ManualBreakpoint();
+	//	ManualBreakpoint();
 		AlternativePackageLocations::RecordModule(hModule);
 
 		ModAPI::AddPostInitFunction(Initialize);
