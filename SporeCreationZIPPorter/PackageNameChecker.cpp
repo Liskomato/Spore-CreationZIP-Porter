736287c6352eac4f31d53ef7442f76d6523dac5e
Tenderi4 <71285540+Tenderi4@users.noreply.github.com>
2023-10-06 11:37:06 +0300

    Updated the code to accommodate for prop files.
    This is part of the new system where I can add and remove stuff being checked in actual prop files without actually editing the code itself. The old methods will remain as fallback code.

--- SporeCreationZIPPorter/PackageNameChecker.cpp
+++ SporeCreationZIPPorter/PackageNameChecker.cpp
@@ -4,6 +4,20 @@
 
 bool PackageNameChecker::IsDatabaseVanilla(Resource::Database* database) {
 	eastl::string16 databaseLocation = database->GetLocation();
+	PropertyListPtr resource;
+	size_t size = 512;
+	eastl::string16* databases = new eastl::string16[size];
+	if (PropManager.GetPropertyList(0xe1547e20, 0x863ef776, resource) &&
+		App::Property().GetArrayString16(resource.get(), 0xe1547e20,size,databases)) {
+		for (int i = 0; i < size; i++) {
+			if (databaseLocation.find(databases[i]) != eastl::string16::npos) {
+				delete[] databases;
+				return true;
+			}
+		}
+
+	}
+	delete[] databases;
 	for each (const auto & packageName in vanillaDatabases) {
 		if (databaseLocation.find(packageName) != eastl::string16::npos) {
 			return true;
@@ -13,12 +27,19 @@ bool PackageNameChecker::IsDatabaseVanilla(Resource::Database* database) {
 }
 
 eastl::string16 PackageNameChecker::IdToString(uint32_t id) {
+	PropertyListPtr resource;
+	eastl::string16 idString = u"";
+	if (PropManager.GetPropertyList(0xbcafbdd4, 0x863ef776,resource) &&
+		App::Property().GetString16(resource.get(),id,idString)) {
+		return idString;
+	}
+	
 	for each (const auto& key in idMap) {
 		if (key.first == id) {
 			return key.second;
 		}
 	}
-	eastl::string16 idString = u"";
+	
 	idString.append_sprintf(u"0x%X",id);
 	return idString;
 }
